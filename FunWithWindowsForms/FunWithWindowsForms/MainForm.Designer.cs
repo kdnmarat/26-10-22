47aef6836f8c0ea5c6e9d9a247d5abf8184d65bf
Marat Kudinov <marat.kudinov@orioninc.com>
2022-10-27 23:14:23 +0200

    Calculations are implemented

--- FunWithWindowsForms/FunWithWindowsForms/MainForm.Designer.cs
+++ FunWithWindowsForms/FunWithWindowsForms/MainForm.Designer.cs
@@ -29,16 +29,16 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.tbRadius = new System.Windows.Forms.TextBox();
-            this.tbLength = new System.Windows.Forms.TextBox();
             this.tbArea = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.tbLength = new System.Windows.Forms.TextBox();
+            this.tbRadius = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -59,6 +59,63 @@
             this.panel1.Size = new System.Drawing.Size(209, 453);
             this.panel1.TabIndex = 0;
             // 
+            // tbArea
+            // 
+            this.tbArea.Location = new System.Drawing.Point(42, 170);
+            this.tbArea.Name = "tbArea";
+            this.tbArea.Size = new System.Drawing.Size(150, 20);
+            this.tbArea.TabIndex = 5;
+            this.tbArea.Text = "0";
+            this.tbArea.TextChanged += new System.EventHandler(this.tbArea_TextChanged);
+            this.tbArea.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbCommon_KeyPress);
+            // 
+            // tbLength
+            // 
+            this.tbLength.Location = new System.Drawing.Point(42, 109);
+            this.tbLength.Name = "tbLength";
+            this.tbLength.Size = new System.Drawing.Size(150, 20);
+            this.tbLength.TabIndex = 4;
+            this.tbLength.Text = "0";
+            this.tbLength.TextChanged += new System.EventHandler(this.tbLength_TextChanged);
+            this.tbLength.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbCommon_KeyPress);
+            // 
+            // tbRadius
+            // 
+            this.tbRadius.Location = new System.Drawing.Point(42, 48);
+            this.tbRadius.Name = "tbRadius";
+            this.tbRadius.Size = new System.Drawing.Size(150, 20);
+            this.tbRadius.TabIndex = 3;
+            this.tbRadius.Text = "0";
+            this.tbRadius.TextChanged += new System.EventHandler(this.tbRadius_TextChanged);
+            this.tbRadius.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbCommon_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "S:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "C:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "R:";
+            // 
             // panel2
             // 
             this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -73,63 +130,15 @@
             this.panel2.Size = new System.Drawing.Size(271, 453);
             this.panel2.TabIndex = 1;
             // 
-            // label1
+            // label6
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 51);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(18, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "R:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 112);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(17, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "C:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(19, 173);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(17, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "S:";
-            // 
-            // tbRadius
-            // 
-            this.tbRadius.Location = new System.Drawing.Point(42, 48);
-            this.tbRadius.Name = "tbRadius";
-            this.tbRadius.Size = new System.Drawing.Size(150, 20);
-            this.tbRadius.TabIndex = 3;
-            // 
-            // tbLength
-            // 
-            this.tbLength.Location = new System.Drawing.Point(42, 109);
-            this.tbLength.Name = "tbLength";
-            this.tbLength.Size = new System.Drawing.Size(150, 20);
-            this.tbLength.TabIndex = 4;
-            // 
-            // tbArea
-            // 
-            this.tbArea.Location = new System.Drawing.Point(42, 170);
-            this.tbArea.Name = "tbArea";
-            this.tbArea.Size = new System.Drawing.Size(150, 20);
-            this.tbArea.TabIndex = 5;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(14, 47);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 19);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Radius";
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(14, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 38);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Circle area\r\nS = πr²";
             // 
             // label5
             // 
@@ -141,15 +150,15 @@
             this.label5.TabIndex = 1;
             this.label5.Text = "Circumference (length of the circle)\r\nC = 2πr";
             // 
-            // label6
+            // label4
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.Location = new System.Drawing.Point(14, 160);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(79, 38);
-            this.label6.TabIndex = 2;
-            this.label6.Text = "Circle area\r\nS = πr²";
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(14, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 19);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Radius";
             // 
             // MainForm
             // 
@@ -158,7 +167,6 @@
             this.ClientSize = new System.Drawing.Size(511, 478);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.panel1.ResumeLayout(false);
--- FunWithWindowsForms/FunWithWindowsForms/MainForm.cs
+++ FunWithWindowsForms/FunWithWindowsForms/MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace FunWithWindowsForms
 {
@@ -15,6 +16,81 @@ namespace FunWithWindowsForms
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void tbCommon_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // accept only digits and points
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != 8 && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+            System.Windows.Forms.TextBox tb = (System.Windows.Forms.TextBox)sender;
+            // but only one point, and not at the beginning of the string
+            if (e.KeyChar == '.' && (tb.Text.Contains('.') || (tb.SelectionStart == 0)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void tbRadius_TextChanged(object sender, EventArgs e)
+        {
+            System.Windows.Forms.TextBox tb = (System.Windows.Forms.TextBox)sender;
+            // to not have problems with empty field
+            if (tb.Text.Length == 0)
+            {
+                tb.Text = "0";
+                return;
+            }
+            double r = Convert.ToDouble(tb.Text);
+            double c = 2 * Math.PI * r;
+            double a = Math.PI * r * r;
+            // to prevent loops
+            if (tb.Focused)
+            {
+                tbLength.Text = c.ToString("0.####");
+                tbArea.Text = a.ToString("0.####");
+            }
+        }
+
+        private void tbLength_TextChanged(object sender, EventArgs e)
+        {
+            System.Windows.Forms.TextBox tb = (System.Windows.Forms.TextBox)sender;
+            // to not have problems with empty field
+            if (tb.Text.Length == 0)
+            {
+                tb.Text = "0";
+                return;
+            }
+            double c = Convert.ToDouble(tb.Text);
+            double r = c / (2 * Math.PI);
+            double a = c * c / (4 * Math.PI);
+            // to prevent loops
+            if (tb.Focused)
+            {
+                tbRadius.Text = r.ToString("0.####");
+                tbArea.Text = a.ToString("0.####");
+            }
+        }
+
+        private void tbArea_TextChanged(object sender, EventArgs e)
+        {
+            System.Windows.Forms.TextBox tb = (System.Windows.Forms.TextBox)sender;
+            // to not have problems with empty field
+            if (tb.Text.Length == 0)
+            {
+                tb.Text = "0";
+                return;
+            }
+            double a = Convert.ToDouble(tb.Text);
+            double r = Math.Sqrt(a / Math.PI);
+            double c = Math.Sqrt(4 * a * Math.PI);
+            // to prevent loops
+            if (tb.Focused)
+            {
+                tbRadius.Text = r.ToString("0.####");
+                tbLength.Text = c.ToString("0.####");
+            }
         }
     }
 }
